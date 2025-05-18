8d730462681b040b35405b5a821e50ee1a62b0eb
Elies García Alvira <68759092+alias313@users.noreply.github.com>
2025-05-18 20:22:01 +0200

    Afegeix la practica 4 de tardor del 2024 (#1)
    * Enunciado
    
    * Init project
    
    * Primer apartat fet
    
    * Segon apartat fet
    
    * Reset outputs on nrst
    
    * Tercer apartat fet
    
    * Offset clk to 0 so it counts 11 'add' pulses

---
+++ t_cnt.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity t_cnt is
+	port( clk, nrst : in std_logic;
+		ara : out std_logic;
+		res : out std_logic_vector(3 downto 0) );
+end t_cnt;
+
+architecture arq of t_cnt is
+signal time_cnt : integer range 0 to 10;
+begin
+	process (clk,nrst) begin
+		if nrst='0' then time_cnt <= 0;
+		elsif clk'event and clk='1' then
+			if time_cnt = 11 then time_cnt <= 1;
+			else time_cnt <= time_cnt+1;
+			end if;
+		end if;
+	end process;
+	ara <= '1' when time_cnt=11 else '0';
+	res <= std_logic_vector(to_unsigned(time_cnt, res'length));
+end arq;
