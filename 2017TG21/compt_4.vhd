f55286a753f0fb025e255387737db09f53c2b37d
alias313 <eliesgarciaalvira@gmail.com>
2025-05-20 15:39:42 +0200

    tc evaluated conditionally now

--- 2017TG21/compt_4.vhd
+++ 2017TG21/compt_4.vhd
@@ -9,38 +9,27 @@ entity compt_4 is port(
 end compt_4;
 
 architecture a of compt_4 is
-signal qq : std_logic_vector(3 downto 0) := x"0";
+	signal qq : std_logic_vector(3 downto 0) := x"0";
 begin
 	process(clk, rst)
 	begin
-		if rst = '1' then qq <= x"0"; tc <= '0';
+		if rst = '1' then qq <= x"0";
 		elsif clk'event and clk = '1' then
 			if (ena = '1' and up = '1') then
 				if qq = x"F" then
 					qq <= x"0";
-					tc <= '0';
 				else
-					if qq = x"E" then
-						tc <= '1';
-					else
-						tc <= '0';
-					end if;
 					qq <= qq+1;
 				end if;
 			elsif (ena = '1' and up = '0') then
 				if qq = x"0" then
 					qq <= x"F";
-					tc <= '0';
 				else
-					if qq = x"1" then
-						tc <= '1';
-					else
-						tc <= '0';
-					end if;
 					qq <= qq-1;
 				end if;
 			end if;
 		end if;
 	end process;
+	tc <= '1' when (ena = '1' and ((up = '1' and qq = x"F") or (up = '0' and qq = x"0"))) else '0';
 	q <= qq;
 end a;
